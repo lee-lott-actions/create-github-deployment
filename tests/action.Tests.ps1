cf3590f4d2f65ec8d52d9c73cf3daabe0e68d6b2
Lee Lott <168018332+Lee-Lott@users.noreply.github.com>
2026-06-05 17:12:33 -0500

    ci: Add CI Tests
    Issue: #1

--- tests/action.Tests.ps1
+++ tests/action.Tests.ps1
@@ -1,10 +1,12 @@
-Describe "Function-Name" {
+Describe "New-Deployment" {
     BeforeAll {
-        $script:Input1   = "input-1"
-        $script:Input2   = "input-2"
-        $script:Input3   = "input-3"
-        $script:Token      = "fake-token"
-        $script:MockApiUrl = "http://127.0.0.1:3000"
+        $script:Ref           = "v1.2.3"
+        $script:Environment   = "production"
+        $script:Description   = "This is a description of the deployment"
+		$script:OrgName       = "test-org"
+		$script:RepoName      = "test-repo"
+        $script:Token         = "fake-token"
+        $script:MockApiUrl    = "http://127.0.0.1:3000"
         . "$PSScriptRoot/../action.ps1"
     }
 
@@ -19,69 +21,87 @@ Describe "Function-Name" {
     }
 
 	Context "Success Cases" {
-	    It "unit: Function-Name succeeds with HTTP 200" {
+	    It "unit: New-Deployment succeeds with HTTP 201" {
 	        Mock Invoke-WebRequest {
-	            [PSCustomObject]@{ StatusCode = 200; Content = '{}' }
+	            [PSCustomObject]@{ StatusCode = 201; Content = '{"id": "123", "url": "http://127.0.0.1/deployments/123"}' }
 	        }
-	        Function-Name -Input1 $Input1 -Input2 $Input2 -Input3 $Input3 -Token $Token 
+			
+	        New-Deployment -Ref $Ref -Environment $Environment -Description $Description -OrgName $OrgName -RepoName $RepoName -Token $Token
 	        $output = Get-Content $env:GITHUB_OUTPUT
 	        $output | Should -Contain "result=success"
+			$output | Should -Contain "deployment_id=123"
+			$output | Should -Contain "deployment_url=http://127.0.0.1/deployments/123"
 	    }
 	}
 
 	Context "HTTP Failure Cases" {
-	    It "unit: Function-Name fails with HTTP 404" {
+	    It "unit: New-Deployment fails with HTTP 404" {
 	        Mock Invoke-WebRequest {
 	            [PSCustomObject]@{ StatusCode = 404; Content = '{"message":"Repository not found"}' }
 	        }
-	        Function-Name -Input1 $Input1 -Input2 $Input2 -Input3 $Input3 -Token $Token 
+			
+	        New-Deployment -Ref $Ref -Environment $Environment -Description $Description -OrgName $OrgName -RepoName $RepoName -Token $Token
 	        $output = Get-Content $env:GITHUB_OUTPUT
 	        $output | Should -Contain "result=failure"
-	        $output | Should -Contain "error-message=Error: Call Failed. HTTP Status: 404"
+	        $output | Should -Contain "error-message=Error: Deployment creation failed. Status code: 404"
 	    }
 	}
 
 	Context "Parameter Validation Failure Cases" {
-	    It "unit: Function-Name fails with empty Input1" {
-	        Function-Name -Input1 "" -Input2 $Input2 -Input3 $Input3 -Token $Token
+	    It "unit: New-Deployment fails with empty Ref" {
+	        New-Deployment -Ref "" -Environment $Environment -Description $Description -OrgName $OrgName -RepoName $RepoName -Token $Token
 	        $output = Get-Content $env:GITHUB_OUTPUT
 	        $output | Should -Contain "result=failure"
-	        $output | Should -Contain "error-message=Missing required parameters: Input1, Input2, and Input3 must be provided."
+	        $output | Should -Contain "error-message=Missing required parameters: Ref, Environment, Description, OrgName, RepoName, and Token must be provided."
 	    }
 
-	    It "unit: Function-Name fails with empty Input2" {
-	        Function-Name -Input1 $Input1 -Input2 "" -Input3 $Input3 -Token $Token
+	    It "unit: New-Deployment fails with empty Environment" {
+	        New-Deployment -Ref $Ref -Environment "" -Description $Description -OrgName $OrgName -RepoName $RepoName -Token $Token
 	        $output = Get-Content $env:GITHUB_OUTPUT
 	        $output | Should -Contain "result=failure"
-	        $output | Should -Contain "error-message=Missing required parameters: Input1, Input2, and Input3 must be provided."
+	        $output | Should -Contain "error-message=Missing required parameters: Ref, Environment, Description, OrgName, RepoName, and Token must be provided."
 	    }
       
-      It "unit: Function-Name fails with empty Input3" {
-	        Function-Name -Input1 $Input1 -Input2 $Input2 -Input3 "" -Token $Token
+    	It "unit: New-Deployment fails with empty Description" {
+	        New-Deployment -Ref $Ref -Environment $Environment -Description "" -OrgName $OrgName -RepoName $RepoName -Token $Token
 	        $output = Get-Content $env:GITHUB_OUTPUT
 	        $output | Should -Contain "result=failure"
-	        $output | Should -Contain "error-message=Missing required parameters: Input1, Input2, and Input3 must be provided."
+	        $output | Should -Contain "error-message=Missing required parameters: Ref, Environment, Description, OrgName, RepoName, and Token must be provided."
 	    }
-      	
-	    It "unit: Function-Name fails with empty Token" {
-	        Function-Name -RepoName $RepoName -Token ""
+
+    	It "unit: New-Deployment fails with empty OrgName" {
+	        New-Deployment -Ref $Ref -Environment $Environment -Description $Description -OrgName "" -RepoName $RepoName -Token $Token
 	        $output = Get-Content $env:GITHUB_OUTPUT
 	        $output | Should -Contain "result=failure"
-	        $output | Should -Contain "error-message=Missing required parameters: Input1, Input2, and Input3 must be provided."
+	        $output | Should -Contain "error-message=Missing required parameters: Ref, Environment, Description, OrgName, RepoName, and Token must be provided."
+	    }
+
+    	It "unit: New-Deployment fails with empty RepoName" {
+	        New-Deployment -Ref $Ref -Environment $Environment -Description $Description -OrgName $OrgName -RepoName "" -Token $Token
+	        $output = Get-Content $env:GITHUB_OUTPUT
+	        $output | Should -Contain "result=failure"
+	        $output | Should -Contain "error-message=Missing required parameters: Ref, Environment, Description, OrgName, RepoName, and Token must be provided."
+	    }		
+		      	
+	    It "unit: New-Deployment fails with empty Token" {
+	        New-Deployment -Ref $Ref -Environment $Environment -Description $Description -OrgName $OrgName -RepoName $RepoName -Token ""
+	        $output = Get-Content $env:GITHUB_OUTPUT
+	        $output | Should -Contain "result=failure"
+	        $output | Should -Contain "error-message=Missing required parameters: Ref, Environment, Description, OrgName, RepoName, and Token must be provided."
 	    }
 	}
 
 	Context "Exception Failure Cases" {
-		It "unit: Function-Name fails with exception" {
+		It "unit: New-Deployment fails with exception" {
 			Mock Invoke-WebRequest { throw "API Error" }
 	
 			try {
-				Function-Name -Input1 $Input1 -Input2 $Input2 -Input3 $Input3 -Token $Token 
+				New-Deployment -Ref $Ref -Environment $Environment -Description $Description -OrgName $OrgName -RepoName $RepoName -Token $Token
 			} catch {}
 	
 			$output = Get-Content $env:GITHUB_OUTPUT
 			$output | Should -Contain "result=failure"
-			$output | Where-Object { $_ -match "^error-message=Error: Call Failed. Exception:" } |
+			$output | Where-Object { $_ -match "^error-message=Error: Deployment creation failed. Exception: " } |
 				Should -Not -BeNullOrEmpty
 		}		
 	}
